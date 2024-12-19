ef40184121cb61ca9bd4c8b2f89680ed4afbd8c7
nbltmm <nabiltamam1604@gmail.com>
2024-12-19 20:22:19 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Validasi menggunakan Bootstrap
+document.addEventListener("DOMContentLoaded", () => {
+    const forms = document.querySelectorAll(".needs-validation");
+
+    Array.from(forms).forEach((form) => {
+        form.addEventListener("submit", (event) => {
+            if (!form.checkValidity()) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+            form.classList.add("was-validated");
+        }, false);
+    });
+});
